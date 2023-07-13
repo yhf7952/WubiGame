1a85ec4f80b0555a161606f9a1e8bb7e9c1074f7
today-calm <todaycalm@protonmail.com>
2023-07-13 11:16:08 +0800

    feat: 增加98五笔版本

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <el-row :class="{ zigen:gameModel == 'zigen' }" class="box" :gutter="20" style="margin-top:50px;">
+        <el-row
+            :class="{ zigen: gameModel === 'zigen', version98: wubiVersion === '98'  }"
+            class="box"
+            :gutter="20"
+            style="margin-top:50px;"
+        >
             <el-col :span="4" :offset="2">
                 <el-card shadow="always" v-bind:class="isRight ? 'boderNormal' :'boderError' " >
                     {{d1[1]}}
@@ -36,7 +41,9 @@
         </el-row>
         <el-collapse value = '1'>
                 <el-collapse-item title="字根图" name="1">
-                    <div class="rootImg"><img src="../assets/root.png" /></div>
+                    <div class="rootImg">
+                        <img :src="require(`../assets/root-${wubiVersion}.png`)" />
+                    </div>
                 </el-collapse-item>
                 <el-collapse-item  title="一级简码" name="2">
                     <el-table style="width: 100%" :data="tableData" >
@@ -48,7 +55,9 @@
                     </el-table>
                 </el-collapse-item>
                 <el-collapse-item title="二级简码" name="3">
-                    <div class="rootImg"><img src="../assets/86erjijianma.gif" /></div>
+                    <div class="rootImg">
+                        <img :src="require(`../assets/${wubiVersion}erjijianma.gif`)" />
+                    </div>
                 </el-collapse-item>
             </el-collapse>
     </div>
@@ -61,8 +70,15 @@
     font-weight: 400;
     src: url("../assets/font/HanZiRootFont.woff2") format("woff2"), url("../assets/font/HanZiRootFont.ttf") format("ttf");
 }
+@font-face{
+    font-family: "98WB";
+    src: url("../assets/font/98WB-1.otf") format("opentype");
+}
 .zigen{
     font-family: HanZiRootFont;
+}
+.zigen.version98 {
+    font-family: "98WB";
 }
 .box{
     font-size: 1cm;
@@ -87,7 +103,7 @@
 </style>
 
 <script>
-    var list = {
+    const list86 = {
         "g":["一","丁","丅","丄","丂","七"],
         "f":["丐","丑","专","丒","丕","丘","丙","世","丗","且"],
         "d":["丠","両","两","严","並","丢","丣","丨","丩","个","丫","丧"],
@@ -113,6 +129,33 @@
         "v":["佰","佱","佲","佴","併","佶","佷","佳"],
         "c":["侀","侄","侅","侃","侁","侂"],
         "x":["侓","侔","侕","侖","侗","侒","侐","侑"],
+    }
+    const list98 = {
+        "g":["王","一","󰁽","󰀞","五","夫","󰀠","󰀟","󰀡","󰂅"],
+        "f":["土","士","干","二","十","󰀛","󰀜","寸","甘","雨","󰀙","未"],
+        "d":["大","犬","戊","三","󰀎","󰂞","古","石","厂","󰂢","󰁼"],
+        "s":["木","󰄉","丁","西","󰄈","甫"],
+        "a":["工","󰀄","匚","戈","󰀃","󰀂","󰀁","七","󰄖","󰂉","廿","󰁺","󰀀"],
+        "h":["目","󰀣","丨","上","󰀥","亅","卜","止","󰁾","󰀤","󰃀"],
+        "j":["日","󰃋","󰀰","󰀱","󰀭","󰀮","󰀯","󰃉","早","虫"],
+        "k":["口","󰀶","川","󰀸"],
+        "l":["田","󰃒","󰀷","甲","车","四","󰃔","󰀹","󰀺","皿"],
+        "m":["山","󰀿","󰃜","由","󰀽","󰀼","贝","󰀾"],
+        "t":["禾","󰁿","󰄋","󰄍","󰄐","󰄎","󰄏"],
+        "r":["白","󰃼","󰁤","斤","丘","气","󰄄","󰃿","󰂀","手"],
+        "e":["月","󰀐","󰂦","󰂥","用","󰀒","力","󰀓","󰂩","毛","󰀔","󰀕","󰀖","󰂪","󰀗"],
+        "w":["人","八","󰄥","󰄧","几","󰁰","󰁯"],
+        "q":["金","󰃵","󰁚","󰃷","夕","󰁝","󰁛","󰁡","󰄾","󰁢","󰁠","儿","󰁜","󰃻"],
+        "y":["言","󰄵","󰄼","󰂃","文","󰁸","󰄹","󰄶","方","󰁷"],
+        "u":["立","󰄕","󰄒","󰄑","󰁦","辛","羊","󰄝","󰁨","󰁩","󰄿","󰄗","六","门","󰁪","󰄛"],
+        "i":["水","󰃁","󰃈","󰃃","󰀪","󰀩","󰃇","󰃆","󰀧","小"],
+        "o":["火","󰃨","业","󰁖","󰁗","广","󰁙","米"],
+        "p":["之","󰃱","󰃰","󰃮","󰃲","󰃯","󰃳"],
+        "n":["已","己","巳","󰁁","󰁂","乙","󰃧","󰁃","󰁄","󰁅","尸","心","󰃣","󰃤","羽"],
+        "b":["子","孑","了","󰄽","耳","󰂏","󰂕","󰁹","󰀇","󰂑","也","乃","󰂔","皮"],
+        "v":["女","󰄞","刀","九","󰄣","󰁮","󰁭","󰁬","󰄢"],
+        "c":["又","󰀉","󰁻","󰂙","巴","󰂛","󰀊"],
+        "x":["幺","󰁲","󰄪","母","󰄰","󰄯","弓","󰁳","󰁴","匕"],
     }
     var listYi = {
         "g":["一"],
@@ -142,8 +185,11 @@
         "x":["经"],
     }
 
-    var strEr="五于天末开下理事画现玫珠表珍列玉平不来与屯妻到互二寺城霜载直是吉协南才垢圾夫无坟增示赫过志地雪支三夺大厅左丰百右历成帮原胡春克太磁砂灰达成顾肆友龙本村枯林械相查可楞机格析极检构术样档杰棕杨李要权楷七革基苛式牙划或功贡攻匠菜共区芳燕东芝世节切芭药睛睦盯虎止旧占卤贞睡肯具餐眩瞳步眯瞎卢眼皮此量时晨果虹早昌蝇曙遇昨蝗明蛤晚景暗晃显晕电最归紧昆呈叶顺呆呀中虽吕另员呼听吸只史嘛啼吵喧叫啊哪吧哟车轩因困四辊加男轴力斩胃办罗罚较边思轨轻累同财央朵曲由则崭册几贩骨内风凡赠峭迪岂邮凤生行知条长处得各力向笔物秀答称入科秒管秘季委么第后持拓打找年提扣押抽手折扔失换扩拉朱搂近所报扫反批且肝肛胆肿肋肌用遥朋脸胸及胶膛爱甩服妥肥脂全会估休代个介保佃仙作伯仍从你信们偿伙亿他分公化钱针然钉氏外旬名锣负儿铁角欠多久匀乐炙锭包凶争色主计庆订度让刘训为高放诉衣认义方说就变这记离良充率闰半关亲并站间部曾商产瓣前闪交六立冰普帝决闻妆冯北汪法尖洒江小浊澡渐没少泊肖兴光注洋水淡学沁池当汉涨业灶类灯煤粘烛炽烟灿烽煌粗伙炮米料炒炎迷断籽娄烃定守害宁宽寂审宫军宙客宾家空宛社实宵灾之官字安它怀导居民收慢避惭届必怕愉懈心习悄屡忱忆敢恨怪尼卫际承阿陈耻阳职阵出降孤阴队隐防联孙耿辽也子限取陛姨寻姑杂毁旭如舅九奶婚妨嫌录灵巡刀好妇妈姆对参戏台劝观矣牟能难允驻驼马邓艰双线结顷红引旨强细纲张绵级给约纺弱纱继综纪弛绿经比";
-    var listEr = strEr.split('');
+    const strEr86 = "五于天末开下理事画现玫珠表珍列玉平不来与屯妻到互二寺城霜载直是吉协南才垢圾夫无坟增示赫过志地雪支三夺大厅左丰百右历成帮原胡春克太磁砂灰达成顾肆友龙本村枯林械相查可楞机格析极检构术样档杰棕杨李要权楷七革基苛式牙划或功贡攻匠菜共区芳燕东芝世节切芭药睛睦盯虎止旧占卤贞睡肯具餐眩瞳步眯瞎卢眼皮此量时晨果虹早昌蝇曙遇昨蝗明蛤晚景暗晃显晕电最归紧昆呈叶顺呆呀中虽吕另员呼听吸只史嘛啼吵喧叫啊哪吧哟车轩因困四辊加男轴力斩胃办罗罚较边思轨轻累同财央朵曲由则崭册几贩骨内风凡赠峭迪岂邮凤生行知条长处得各力向笔物秀答称入科秒管秘季委么第后持拓打找年提扣押抽手折扔失换扩拉朱搂近所报扫反批且肝肛胆肿肋肌用遥朋脸胸及胶膛爱甩服妥肥脂全会估休代个介保佃仙作伯仍从你信们偿伙亿他分公化钱针然钉氏外旬名锣负儿铁角欠多久匀乐炙锭包凶争色主计庆订度让刘训为高放诉衣认义方说就变这记离良充率闰半关亲并站间部曾商产瓣前闪交六立冰普帝决闻妆冯北汪法尖洒江小浊澡渐没少泊肖兴光注洋水淡学沁池当汉涨业灶类灯煤粘烛炽烟灿烽煌粗伙炮米料炒炎迷断籽娄烃定守害宁宽寂审宫军宙客宾家空宛社实宵灾之官字安它怀导居民收慢避惭届必怕愉懈心习悄屡忱忆敢恨怪尼卫际承阿陈耻阳职阵出降孤阴队隐防联孙耿辽也子限取陛姨寻姑杂毁旭如舅九奶婚妨嫌录灵巡刀好妇妈姆对参戏台劝观矣牟能难允驻驼马邓艰双线结顷红引旨强细纲张绵级给约纺弱纱继综纪弛绿经比";
+    const listEr86 = strEr86.split('');
+
+    const strEr98 = "五于天末开下理事画现麦珀表珍万玉来求亚琛与击妻到互十寺城某域直刊吉雷南才垢协零无坊增示赤过志坡雪支坶三夺大厅左还百右面而故原历其克太辜砂矿达成破肆友龙本票顶林模相查可柬贾枚析杉机构术样档杰枕札李根权楷七革苦莆式牙划或苗贡攻区功共匹芳蒋东蘑芝艺节切芭药睛睦非盯瞒步旧占卤贞睡睥肯具餐虔瞳叔虚瞎虑眼眸此量时晨果晓早昌蝇曙遇鉴蚯明蛤晚影暗晃显蛇电最归坚昆号叶顺呆呀足虽吕喂员吃听另只兄唁咬吵嘛喧叫啊啸吧哟车团因困轼四辊回田轴略斩男界罗罚较辘连思囝轨轻累赋财央崧曲由则迥崭册败冈骨内见丹赠峭赃迪岂邮峻幽年等知条长处得各备身秩稀务答稳入冬秒秋乏乐秀委么每后质拓打找看提扣押抽手折拥兵换搞拉泉扩近所报扫反指且肚须采肛毡胆加舆觅用貌朋办胸肪胶膛脏边力服妥肥脂全什估休代个介保佃仙八风佣从你信们偿伙伫亿他分公化钱针然钉氏外旬名甸负儿勿角欠多久匀尔炙锭包迎争色锴证计诚订试让刘训亩市放义衣认询方详就亦亮记享良充率半斗头亲并着间问闸端道交前闪次六立冰普闷疗妆痛北光汗尖浦江小浊溃泗油少汽肖没沟济洋水渡党沁波当汉涨精庄类床席业烛燥库灿庭粕粗府底广粒应炎迷断籽数序鹿家守害宁赛寂审宫军宙客宾农空宛社实宵灾之官字安它那导居懒异收慢避惭届改怕尾恰懈心习尿屡忱已敢恨怪尼卫际承阿陈耻阳职阵出降孤阴队陶及联孙耿辽也子限取陛建寻姑杂既肃旭如姻妯九婢姐妗婚妨嫌录灵退恳好妇妈姆马对参牺戏犋台观矣能难物叉予邓艰双牝线结顷缚红引旨强细贯乡绵组给约纺弱纱继综纪极绍弘比";
+    const listEr98 = strEr98.split('');
     
     //console.log(getRandom());
 
@@ -172,6 +218,7 @@
         },
         props:{
             gameModel: String,
+            wubiVersion: String,
         },
         computed: {
             //计算打字速度
@@ -186,18 +233,18 @@
         },
         watch:{
             gameModel(val){
-                this.d1='';
-                this.d2='';
-                this.d3='';
-                this.d4='';
-                this.d5='';
-                this.getData();
-                this.reset();
+                this.startNewGame();
                 if(val == "erji"){
                     this.$message('当前是二级简码模式，请更换为五笔输入法练习');
                 }
-                document.getElementById("intext").focus();
-            }
+            },
+            wubiVersion(val) {
+                this.startNewGame();
+                this.$message({
+                    message: `已切换为 ${val} 版本`,
+                    type: 'success',
+                });
+            },
         },
         methods: {
             //初始化
@@ -242,30 +289,44 @@
             },
             reset(){
                 this.rightTimes=0;
+                this.errorTimes = 0;
                 this.errorSumTimes = 0;
                 this.lastRightDate = new Date();
                 this.isRight = true;
                 document.getElementById("intext").focus();
             },
+            startNewGame() {
+                this.d1='';
+                this.d2='';
+                this.d3='';
+                this.d4='';
+                this.d5='';
+                this.getData();
+                this.reset();
+            },
             getRandom(){
-                var thisList = list;
+                var thisList = {};
+                const isVersion86 = this.wubiVersion === '86';
+
                 switch(this.gameModel){
-                    case "zigen":
+                    case "zigen": {
+                        const list = isVersion86 ? list86 : list98;
                         thisList = list;
                         break;
+                    }
                     case "yiji":
                         thisList = listYi;
                         break;
-                    case "erji":
+                    case "erji": {
+                        const listEr = isVersion86 ? listEr86 : listEr98;
                         return ["",listEr[Math.floor(Math.random() * listEr.length)]];
+                    }
                 }
                 
-                var returnVal = {};
                 var keyList = Object.keys(thisList);
                 var key = keyList[Math.floor(Math.random() * keyList.length)];
                 var values = thisList[key];
                 var value = values[Math.floor(Math.random() * values.length)];
-                returnVal[key]=value;
                 return [key,value];
             }
 
